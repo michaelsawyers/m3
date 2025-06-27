d84b0b42ad61509ebd60d4c1634fc8872020b227
Mike Sawyers <michael.sawyers@abbvie.com>
2025-06-27 15:41:33 -0500

    fix: registry types

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import * as React from "react";
 import { OpenInV0Button } from "@/components/open-in-v0-button";
+import { HelloWorld } from "@/registry/new-york/blocks/hello-world/hello-world";
+import { ExampleForm } from "@/registry/new-york/blocks/example-form/example-form";
+import PokemonPage from "@/registry/new-york/blocks/complex-component/page";
+import { ExampleCard } from "@/registry/new-york/blocks/example-with-css/example-card";
 import { Scaffold } from "@/registry/unity/blocks/scaffold";
 import { AkHeader } from "@/registry/unity/ui/ak-header";
 // This page displays items from the custom registry.
@@ -26,7 +31,7 @@ export default function Home() {
           </div>
         </div>
 
-        {/* <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
+        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
           <div className="flex items-center justify-between">
             <h2 className="text-sm text-muted-foreground sm:pl-3">
               A simple hello world component
@@ -72,7 +77,7 @@ export default function Home() {
           <div className="flex items-center justify-center min-h-[400px] relative">
             <ExampleCard />
           </div>
-        </div> */}
+        </div>
       </main>
     </div>
   );
